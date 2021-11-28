909b41c68d77839198f76bcfa3003bdca70f07ef
ebedinin <bedinin@gmail.com>
2021-11-28 22:15:26 +1000

    27

--- learn.js
+++ learn.js
@@ -1,7 +1,7 @@
-function Dog() {
-    this.name = "Rupert";
-    this.color = "brown";
+function Dog(name,color) {
+    this.name = name;
+    this.color = color;
     this.numLegs = 4;
 }
 
-const hound = new Dog();
+const terrier = new Dog("a","b");
