63e513b4f27589fcc3d4702e1c9f8c6712ed94a1
ebedinin <bedinin@gmail.com>
2021-11-29 22:45:05 +1000

    46

--- learn.js
+++ learn.js
@@ -2,19 +2,10 @@ function Dog(name) {
     this.name = name;
 }
 
-Dog.prototype.numLegs = 4;
-
-let beagle = new Dog("Snoopy");
-
-let ownProps = [];
-let prototypeProps = [];
-for(let item in beagle){
-    if (beagle.hasOwnProperty(item)){
-        ownProps.push(item)
-    }
-    else{
-        prototypeProps.push(item)
-    }
-}
-console.log(ownProps)
 // Only change code below this line
+function joinDogFraternity(candidate) {
+    if (candidate.constructor === Dog)
+        return true;
+    else
+        return false
+}
