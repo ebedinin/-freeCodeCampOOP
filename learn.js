6eda5e04b8c77516096fa59fef0a5a27f541305e
ebedinin <bedinin@gmail.com>
2021-11-29 23:07:11 +1000

    65

--- learn.js
+++ learn.js
@@ -4,5 +4,7 @@ function Dog(name) {
 
 let beagle = new Dog("Snoopy");
 
-Dog.prototype.isPrototypeOf(beagle);
-// Only change code below this line
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
