bc30fe77df508356a1fef6195738b283045d12ad
ebedinin <bedinin@gmail.com>
2021-12-02 21:41:06 +1000

    92

--- learn.js
+++ learn.js
@@ -1,14 +1,5 @@
-let bird = {
-    name: "Donald",
-    numLegs: 2
-};
+function Bird() {
+    let weight = 15;
 
-let boat = {
-    name: "Warrior",
-    type: "race-boat"
-};
-
-const glideMixin = (obj)=> obj.glide = ()=>console.log("Flying, wooosh!");
-// Only change code below this line
-glideMixin(bird);
-glideMixin(boat);
+    this.getWeight = ()=> weight;
+}
